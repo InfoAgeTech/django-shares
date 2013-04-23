ddebb51df164fd4eff9add0158c8026140b6c7d2
Troy Grosfield <troy.grosfield@gmail.com>
2013-04-22 21:46:34 -0600

    First steps for converting to resuable sharing app.

--- django_sharing/mixins.py
+++ django_sharing/mixins.py
@@ -1,34 +1,146 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
-from mongoengine.fields import EmbeddedDocumentField
-from mongoengine.fields import ListField
-from mongo_sharing.documents.embedded import BaseShareEmbeddedDocument
-from mongo_sharing.documents.embedded import BaseSharePendingEmbeddedDocument
+from django.db import models
+from django_sharing.models import SharePending
+from django.contrib.contenttypes import generic
+from django_sharing.models import Share
+
+# class SimpleDocShareMixin(object):
+#    """Sharing mixing for a document.
+#
+#    Fields:
+#
+#    * share_user_ids: the current list of user ids sharing this object.
+#
+#    """
+#    share_user_ids = ListField(db_field='suids')
+#
+#
+#    def add_share(self, for_user_id, additional_updates=None, auto_save=True):
+#        """Adds a share to a document.
+#
+#        :param for_user_id: id of the user sharing this document.
+#        :param auto_save: boolean indicating if the document should make a call
+#            to mongo to add the share to the document. If false, just appends
+#            the share to self.
+#
+#        """
+#        self.add_shares(for_user_ids=[for_user_id],
+#                        additional_updates=additional_updates,
+#                        auto_save=auto_save)
+#
+#    def add_shares(self, for_user_ids, additional_updates=None, auto_save=True):
+#        """Adds many user shares to the to the doc class.
+#
+#        :param additional_updates: additional updates to make on the selected
+#            documents.
+#
+#        """
+#        if auto_save:
+#
+#            updates = {'add_to_set__share_user_ids': for_user_ids}
+#
+#            if additional_updates:
+#                updates.update(**additional_updates)
+#
+#            self.__class__.objects(id=self.id).update_one(**updates)
+#            self.reload()
+#        else:
+#            for user_id in for_user_ids:
+#                if user_id not in self.share_user_ids:
+#                    self.share_user_ids.append(user_id)
+#
+#    def is_shared_by_user(self, for_user_id):
+#        """Check to see if this document is shared by a specific user.
+#
+#        Returns a boolean True if this user is sharing this document.
+#        Otherwise, False.
+#
+#        """
+#        if not for_user_id:
+#            return False
+#
+#        return for_user_id in self.share_user_ids
+#
+#    def remove_share(self, for_user_id, additional_updates=None,
+#                     auto_save=True):
+#        """Removes user sharing this doc.  Returns true if the user was removed.
+#        Otherwise, returns False.
+#
+#        """
+#        self.remove_shares(for_user_ids=[for_user_id],
+#                           auto_save=auto_save,
+#                           additional_updates=additional_updates)
+#
+#    def remove_shares(self, for_user_ids, additional_updates=None,
+#                      auto_save=True):
+#        """Removes users sharing this doc.  Returns true if the user was removed.
+#        Otherwise, returns False.
+#
+#        """
+#        if auto_save:
+#            try:
+#                updates = {'pull_all__share_user_ids': for_user_ids}
+#                if additional_updates:
+#                    updates.update(**additional_updates)
+#
+#                self.__class__.objects(id=self.id).update_one(**updates)
+#                self.reload()
+#            except self.__class__.DoesNotExist:
+#                return self
+#        else:
+#            self.share_user_ids = [uid for uid in self.share_user_ids
+#                                   if uid not in for_user_ids]
 
 
-class SimpleDocShareMixin(object):
-    """Sharing mixing for a document.
+class ShareModelMixin(models.Model):
+    """Document Sharing mixing that adds a shares list field which stores 
+    additional metadata about a users share.
     
-    Fields:
-    
-    * share_user_ids: the current list of user ids sharing this object.
+    :param share_embedded_document: this should be a sub class of BaseEmbeddedDocument
+    :param shares: list of current sharing objects for specific users.
     
     """
-    share_user_ids = ListField(db_field='suids')
 
+#    meta = {'share_embedded_document': BaseShareEmbeddedDocument}
+#    shares = ListField(EmbeddedDocumentField(meta['share_embedded_document']),
+#                       db_field='sh')
+    shares = generic.GenericRelation(Share)
 
-    def add_share(self, for_user_id, additional_updates=None, auto_save=True):
-        """Adds a share to a document.
+    class Meta:
+        abstract = True
+
+    def add_share(self, for_user_id, additional_updates=None, auto_save=True,
+                  **share_kwargs):
+        """Adds the user id to the list of sharing user ids and add a share 
+        objects with associated metadata for the 'shares_class' embedded 
+        Document.
         
-        :param for_user_id: id of the user sharing this document.
-        :param auto_save: boolean indicating if the document should make a call 
-            to mongo to add the share to the document. If false, just appends 
-            the share to self.
-            
+        :param for_user_id: the id of the user the share is for.
+        :param additional_updates: dictionary of additional updates to pass to
+            the queryset update method. This is only applicable if 
+            auto_save=True.
+        :param auto_save: if True, will save the document after adding the share.
+            If False, the .save() will have to be explicitly called after 
+            calling this method.  This also allow for many shares to be added to
+            a single bill before saving.
+        :param share_kwargs: the kwargs for a specific user share that are 
+            kwargs for the share_embedded_document.
+        
         """
-        self.add_shares(for_user_ids=[for_user_id],
-                        additional_updates=additional_updates,
-                        auto_save=auto_save)
+        share = self._meta['share_embedded_document'](for_user_id=for_user_id, **share_kwargs)
+
+        if auto_save:
+            if not additional_updates:
+                additional_updates = {}
+
+            additional_updates['add_to_set__shares'] = share
+        else:
+            self.shares.append(share)
+
+        super(ShareModelMixin, self).add_share(for_user_id=for_user_id,
+                                             additional_updates=additional_updates,
+                                             auto_save=auto_save)
 
     def add_shares(self, for_user_ids, additional_updates=None, auto_save=True):
         """Adds many user shares to the to the doc class.
@@ -63,82 +175,6 @@ class SimpleDocShareMixin(object):
 
         return for_user_id in self.share_user_ids
 
-    def remove_share(self, for_user_id, additional_updates=None,
-                     auto_save=True):
-        """Removes user sharing this doc.  Returns true if the user was removed.
-        Otherwise, returns False.
-        
-        """
-        self.remove_shares(for_user_ids=[for_user_id],
-                           auto_save=auto_save,
-                           additional_updates=additional_updates)
-
-    def remove_shares(self, for_user_ids, additional_updates=None,
-                      auto_save=True):
-        """Removes users sharing this doc.  Returns true if the user was removed.
-        Otherwise, returns False.
-        
-        """
-        if auto_save:
-            try:
-                updates = {'pull_all__share_user_ids': for_user_ids}
-                if additional_updates:
-                    updates.update(**additional_updates)
-
-                self.__class__.objects(id=self.id).update_one(**updates)
-                self.reload()
-            except self.__class__.DoesNotExist:
-                return self
-        else:
-            self.share_user_ids = [uid for uid in self.share_user_ids
-                                   if uid not in for_user_ids]
-
-
-class DocShareMixin(SimpleDocShareMixin):
-    """Document Sharing mixing that adds a shares list field which stores 
-    additional metadata about a users share.
-    
-    :param share_embedded_document: this should be a sub class of BaseEmbeddedDocument
-    :param shares: list of current sharing objects for specific users.
-    
-    """
-
-    meta = {'share_embedded_document': BaseShareEmbeddedDocument}
-    shares = ListField(EmbeddedDocumentField(meta['share_embedded_document']),
-                       db_field='sh')
-
-    def add_share(self, for_user_id, additional_updates=None, auto_save=True,
-                  **share_kwargs):
-        """Adds the user id to the list of sharing user ids and add a share 
-        objects with associated metadata for the 'shares_class' embedded 
-        Document.
-        
-        :param for_user_id: the id of the user the share is for.
-        :param additional_updates: dictionary of additional updates to pass to
-            the queryset update method. This is only applicable if 
-            auto_save=True.
-        :param auto_save: if True, will save the document after adding the share.
-            If False, the .save() will have to be explicitly called after 
-            calling this method.  This also allow for many shares to be added to
-            a single bill before saving.
-        :param share_kwargs: the kwargs for a specific user share that are 
-            kwargs for the share_embedded_document.
-        
-        """
-        share = self._meta['share_embedded_document'](for_user_id=for_user_id, **share_kwargs)
-
-        if auto_save:
-            if not additional_updates:
-                additional_updates = {}
-
-            additional_updates['add_to_set__shares'] = share
-        else:
-            self.shares.append(share)
-
-        super(DocShareMixin, self).add_share(for_user_id=for_user_id,
-                                             additional_updates=additional_updates,
-                                             auto_save=auto_save)
-
     def remove_share(self, for_user_id, auto_save=True):
         """Remove share for a user id"""
         self.remove_shares(for_user_ids=[for_user_id],
@@ -162,7 +198,7 @@ class DocShareMixin(SimpleDocShareMixin):
         else:
             self.shares = shares
 
-        super(DocShareMixin, self).remove_shares(for_user_ids=for_user_ids,
+        super(ShareModelMixin, self).remove_shares(for_user_ids=for_user_ids,
                                                  additional_updates=additional_updates,
                                                  auto_save=auto_save)
 
@@ -182,7 +218,7 @@ class DocShareMixin(SimpleDocShareMixin):
                 return share
 
 
-class BasePendingSharesMixin(object):
+class SharePendingMixin(models.Model):
     """Pending sharing mixin use to represent a document that wants to be 
     shared with a user.  The user can either be an existing site user or 
     potential future site user.
@@ -194,10 +230,15 @@ class BasePendingSharesMixin(object):
     * pending_shares: list of pending share objects.
     
     """
-    meta = {'pending_share_embedded_document': BaseSharePendingEmbeddedDocument}
-    pending_shares = ListField(EmbeddedDocumentField(meta['pending_share_embedded_document']),
-                               db_field='ps')
+#    meta = {'pending_share_embedded_document': BaseSharePendingEmbeddedDocument}
+#    pending_shares = ListField(EmbeddedDocumentField(meta['pending_share_embedded_document']),
+#                               db_field='ps')
 
+#    pending_shares = models.ForeignKey(SharePending, null=True, blank=True)
+    pending_shares = generic.GenericRelation(SharePending)
+
+    class Meta:
+        abstract = True
 
     def add_pending_share(self, auto_save=True, **kwargs):
         """Adds a pending share for the doc.
--- django_sharing/models.py
+++ django_sharing/models.py
@@ -1,7 +1,75 @@
 # -*- coding: utf-8 -*-
+from django.contrib.auth import get_user_model
+from django.contrib.contenttypes import generic
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django_tools.models import AbstractBaseModel
+import datetime
+
+User = get_user_model()
 
 
-class Sharing(AbstractBaseModel):
-    """Model that represents sharings"""
+class SharePending(AbstractBaseModel):
+    """This is the base model used for pending requests. 
+    
+    When first created, the id and the token will be the same.  These are two
+    separate fields because the id should never change.  The token could get 
+    reassigned if it expires or a new token needs to be generated at a later 
+    time.
+    
+    Fields:
+    
+    * email: email of the user who the share was sent to if uid is unknown.
+    * first_name: first name of the person invited
+    * last_name: last name of the person invited
+    * last_sent: date time the share was last sent.
+    * created_id: the id of the user who sent the pending share
+    * for_user_id: user_id the pending share is for (optional since they might  
+        not be an actual user yet).
+    * message: message sent to user in email.
+    * token: pending share token key.
+    
+    """
+    email = models.EmailField()
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    last_sent = models.DateTimeField(default=datetime.datetime.utcnow)
+#    for_user = models.ForeignKey(User, blank=True, null=True)
+    message = models.TextField()
+    token = models.CharField()
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.PositiveIntegerField()
+    content_object = generic.GenericForeignKey('content_type', 'object_id')
+
+    # TODO: could add a JSONField here and allow this model to be extended
+    #       and add additional properties to that field.  Then new fields would
+    #       be accessed via
+    #
+    # _sharing_metadata = JSONField()
+    #
+    # @property
+    # def some_attribute_name(self):
+    #    self._sharing_metadata.get('some_attr_name')
+
+
+class Share(AbstractBaseModel):
+    """Base share object represents an embedded document of information for a 
+    specific user sharing a document.
+    
+    Fields:
+    
+    * for_user_id = user id for sharing this instance. 
+    
+    """
+    for_user = models.ForeignKey(User, blank=True, null=True)
+    # TODO: Could use a second generic foreign key here to represent the metadata
+    #       for whatever type of share it was.  So, it's a bill share I would have
+    #       things like amount, percent share, etc on a SomeObjectShareMeta models which
+    #       is what the generic key would reference.
+    #
+    # Or just create a separate model for SomeObjectShare and make this model abstract?
+    #
+    # See third option for a JSONField above (can't query off it's values)
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.PositiveIntegerField()
+    content_object = generic.GenericForeignKey('content_type', 'object_id')
