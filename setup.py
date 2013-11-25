661bf45df86f901d211ca67962c1eea47727e08c
Troy Grosfield <troy.grosfield@gmail.com>
2013-11-25 07:59:35 -0700

    setup file updates

--- setup.py
+++ setup.py
@@ -1,31 +1,40 @@
 # -*- coding: utf-8 -*-
+import os
+
 from setuptools import find_packages
 from setuptools import setup
 
+
 classifiers = [
     'Environment :: Web Environment',
+    'Framework :: Django',
     'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
     'Operating System :: OS Independent',
     'Programming Language :: Python',
-    'Topic :: Software Development :: Libraries :: Python Modules',
+    'Programming Language :: Python :: 2.7',
+    'Topic :: Internet :: WWW/HTTP',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
 ]
 
+README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 
 setup(
     name='django-sharing',
     version='0.0.1',
     description='Sharing for django',
     author='Troy Grosfield',
-    author_email='troy.grosfield@gmail.com',
+    maintainer='Troy Grosfield',
+    long_description=README,
     url='https://github.com/InfoAgeTech/django-sharing',
-    # license='MIT',
+    license='MIT',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     test_suite='nose.collector',
     tests_require=['nose'],
     setup_requires=[
-        'django >= 1.5.1',
+        'django >= 1.5.5',
         'django-nose >= 1.1'
     ],
     classifiers=classifiers
