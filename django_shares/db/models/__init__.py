202813368f0d4eab9e65a04421245426658ccc92
Troy Grosfield <troy.grosfield@gmail.com>
2014-01-31 07:54:36 -0700

    Missed adding the init

---
+++ django_shares/db/models/__init__.py
@@ -0,0 +1,5 @@
+from .managers import ShareManager
+from .managers import SharedObjectManager
+from .mixins import AbstractSafeDeleteSharedObjectModelMixin
+from .mixins import AbstractSharedObjectModelMixin
+from .mixins import SafeDeleteShareModelMixin
